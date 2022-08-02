fb508e067b13f367ab26e341c04fee6bcefdf77a
Tahaa Farooq <tahacodez@gmail.com>
2022-08-02 10:10:51 +0300

    Finished all functionalities

--- neatnote.c
+++ neatnote.c
@@ -7,6 +7,7 @@
 
 #define DATA_SIZE 1000
 
+// function to create a note
 int create_note() {
     char data[DATA_SIZE];
     char filename[20];
@@ -33,9 +34,83 @@ int create_note() {
     return 0;
 }
 
-void edit_note();
-void delete_note();
-void shell();
+// function to edit a note
+int edit_note() {
+    char data[DATA_SIZE];
+    char filename[20];
+    FILE *fPtr;
+
+    printf("Enter File Name : ");
+    scanf("%s", &filename);
+
+    fPtr = fopen(filename, "w");
+
+    if (fPtr == NULL) {
+        printf("Something Went Wrong");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Enter Data To Write : ");
+    fgets(data, DATA_SIZE, stdin);
+    fputs(data, fPtr);
+    fclose(fPtr);
+
+    printf("File Edited");
+
+    return 0;
+}
+
+// function to delete note
+int delete_note() {
+    char filename[20];
+    char command[] = "rm -rf ";
+
+    printf("Enter File Name : ");
+    scanf("%s", &filename);
+
+    system(strcat(command, filename));
+
+    return 0;
+}
+
+// function to spawn a shell
+int shell() {
+    char command[100];
+    int cmdLen, wordLen;
+    int i, j, found;
+    char word[] = "whoami";
+
+    cmdLen = strlen(command);
+    wordLen = strlen(word);
+
+    printf("[1] HELP\n[2] id\n[3] whoami\n[4] hostname\n[5] exit\n[$] ");
+    scanf("%s", command);
+
+    while ( 1 == 1 ) {
+        if (strcmp(command, "id") == 0) {
+            system(command);
+        } else if (strcmp(command, "hostname") == 0) {
+            system(command);
+        } else if (strcmp(command, "exit") == 0) {
+            system(command);
+        } else {
+            exit(0);
+        }
+
+        for (i=0; i < cmdLen - wordLen; i++) {
+            found = 1;
+            for (j=0; j < wordLen; j++) {
+                if (command[i + j] != word[j]) {
+                    found = 0;
+                    break;
+                }
+            }
+            if (found == 1) {
+                system(word);
+            }
+        }
+    }
+}
 
 int main() {
     char username[10];
